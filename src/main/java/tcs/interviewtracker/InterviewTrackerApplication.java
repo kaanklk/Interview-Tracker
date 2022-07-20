3be3272684099529a4d9d386ff10eb2134ac629c
Kaan Kulak <kaankulak00@gmail.com>
2022-07-20 13:21:35 +0200

    initial commit

---
+++ src/main/java/tcs/interviewtracker/InterviewTrackerApplication.java
@@ -0,0 +1,13 @@
+package tcs.interviewtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tcs/interviewtracker/InterviewTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package tcs.interviewtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
