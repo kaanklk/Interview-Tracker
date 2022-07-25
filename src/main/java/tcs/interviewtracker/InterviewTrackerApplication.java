d3c80971813d05e9f7018f2c20aca8837c692886
TheFlyingPiano99 <simonzolika1999@gmail.com>
2022-07-25 12:29:40 +0200

    Separated system-specific properties

--- src/main/java/tcs/interviewtracker/InterviewTrackerApplication.java
+++ src/main/java/tcs/interviewtracker/InterviewTrackerApplication.java
@@ -2,8 +2,14 @@ package tcs.interviewtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+	@PropertySource("classpath:application.properties"),
+	@PropertySource("classpath:system-specific.properties")
+})
 public class InterviewTrackerApplication {
 
 	public static void main(String[] args) {
