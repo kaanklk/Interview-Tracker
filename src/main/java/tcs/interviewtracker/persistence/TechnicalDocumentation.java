bf876f00681acc9f7a24fc9a32b674ca44e4eb56
Krisztian Horvath <krissz@gmail.com>
2022-07-26 10:10:41 +0200

    TechnicalDocumentation class

---
+++ src/main/java/tcs/interviewtracker/persistence/TechnicalDocumentation.java
@@ -0,0 +1,11 @@
+package tcs.interviewtracker.persistence;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class TechnicalDocumentation {
+
+}
