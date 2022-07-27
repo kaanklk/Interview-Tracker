0c15648645f7e7969896ac921c3b577b006598a5
TheFlyingPiano99 <simonzolika1999@gmail.com>
2022-07-27 10:25:27 +0200

    created candidate service class

---
+++ src/main/java/tcs/interviewtracker/service/CandidateService.java
@@ -0,0 +1,17 @@
+package tcs.interviewtracker.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import tcs.interviewtracker.repository.CandidateRepository;
+
+@Service
+@AllArgsConstructor
+public class CandidateService {
+    private CandidateRepository candidateRepository;
+
+    public getById() {
+        
+    }
+
+}
