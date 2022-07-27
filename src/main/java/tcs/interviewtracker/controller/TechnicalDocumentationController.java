5eb4916e6cb27cd0f6f6c68a93772fdb16d5cd3b
Krisztian Horvath <krissz@gmail.com>
2022-07-27 13:05:02 +0200

    Get Post Put Delete technical documentation

--- src/main/java/tcs/interviewtracker/controller/TechnicalDocumentationController.java
+++ src/main/java/tcs/interviewtracker/controller/TechnicalDocumentationController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,5 +41,48 @@ public class TechnicalDocumentationController {
         return ResponseEntity.ok().body(techDoc);
     }
 
-    
+    @PutMapping("/{technicalId}")
+    public ResponseEntity<TechnicalDocumentation> updateTechnicalDocumentation(@PathVariable(value = "technicalId") Long techId, @Validated @RequestBody TechnicalDocumentation techDoc) throws Exception{
+        TechnicalDocumentation technicalDocumentation = techDocRepository.findById(techId)
+        .orElseThrow(() -> new Exception("Technical documentation is not found with this id: "+ techId));
+
+        technicalDocumentation.setCandidate(techDoc.getCandidate());
+        technicalDocumentation.setDate(techDoc.getDate());
+        technicalDocumentation.setDesignationOne(techDoc.getDesignationOne());
+        technicalDocumentation.setDesignationTwo(techDoc.getDesignationTwo());
+        technicalDocumentation.setDuration(techDoc.getDuration());
+        technicalDocumentation.setInterviewerOne(techDoc.getInterviewerOne());
+        technicalDocumentation.setInterviewerTwo(techDoc.getInterviewerTwo());
+        technicalDocumentation.setIsReccomended(techDoc.getIsReccomended());
+        technicalDocumentation.setLastComments(techDoc.getLastComments());
+        technicalDocumentation.setRoleExperience(techDoc.getRoleExperience());
+
+        technicalDocumentation.setTechSkillComment1(techDoc.getTechSkillComment1());
+        technicalDocumentation.setTechSkillComment2(techDoc.getTechSkillComment2());
+        technicalDocumentation.setTechSkillComment3import org.hibernate.engine.transaction.jta.platform.internal.TransactionManagerBasedSynchronizationStrategy;
+        (techDoc.getTechSkillComment3());
+        technicalDocumentation.setTechSkillComment4(techDoc.getTechSkillComment4());
+        
+        technicalDocumentation.setTechnicalSkills1(techDoc.getTechnicalSkills1());
+        technicalDocumentation.setTechnicalSkills2(techDoc.getTechnicalSkills2());
+        technicalDocumentation.setTechnicalSkills3(techDoc.getTechnicalSkills3());
+        technicalDocumentation.setTechnicalSkills4(techDoc.getTechnicalSkills4());
+
+        technicalDocumentation.setTotalExperience(techDoc.getTotalExperience());
+        technicalDocumentation.setUnderstandingComment(techDoc.getUnderstandingComment());
+        technicalDocumentation.setUnderstandingOfRole(techDoc.getUnderstandingOfRole());
+
+
+        TechnicalDocumentation finalTechDoc = techDocRepository.save(technicalDocumentation);
+        return ResponseEntity.ok(finalTechDoc);
+    }
+
+    @DeleteMapping("/{technicalId}")
+    public ResponseEntity<TechnicalDocumentation> deleteTechnicalDocumentation(@PathVariable(value = "technicalId") Long techId ) throws Exception {
+        TechnicalDocumentation technicalDocumentation = techDocRepository.findById(techId)
+        .orElseThrow(()-> new Exception("Technincal documentation is not found with this id: "+techId));
+        techDocRepository.delete(technicalDocumentation);
+        return ResponseEntity.ok(technicalDocumentation); 
+    }
+
 }
