09d2832cea23e70db45d60bb253a81b4b54665ce
shukhratojiev <steven.gerrard007@mail.ru>
2022-07-27 13:24:55 +0200

    updatedUser

--- src/main/java/tcs/interviewtracker/persistence/Role.java
+++ src/main/java/tcs/interviewtracker/persistence/Role.java
@@ -1,10 +1,13 @@
 package tcs.interviewtracker.persistence;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -20,7 +23,7 @@ public class Role {
 
     @Id
     @Column(name="role_id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name="role_name",nullable = false)
