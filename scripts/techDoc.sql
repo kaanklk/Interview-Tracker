6da69f3df83d52e3ae3f24781e10bb1f170cd291
Krisztian Horvath <krissz@gmail.com>
2022-07-28 11:00:01 +0200

    Script

---
+++ scripts/techDoc.sql
@@ -0,0 +1,57 @@
+INSERT INTO technical_documentation VALUES (
+1, '2000.12.12', 1, 2, 120, FALSE, 'This is the end of you', 3, 'Tech skill comment num1',
+	'Tech skill comment num2',
+	'Tech skill comment num3',
+	'Tech skill comment num4',
+	1,2,3,4,
+	8,
+	'I think he does not understand the way of Ukulele',
+	'He understood the role',
+	1, 4
+)
+INSERT INTO technical_documentation VALUES (
+2, '2010.10.10', 1, 4, 170, TRUE, 'This is not the end of you', 5, 'High leveled',
+	'High',
+	'High',
+	'VERY High',
+	2,1,5,1,
+	10,
+	'I think he does understand the way of Ukulele!',
+	'He understood the role',
+	2, 1
+)
+INSERT INTO technical_documentation VALUES (
+3, '2003.03.03', 2, 3, 200, FALSE, 'This is the biggest mistake', 3, 'Low leveled',
+	'He can turn on the computer',
+	'Wonderful',
+	'VERY High',
+	1,4,3,5,
+	2,
+	'Actually he can play the guitar',
+	'He does not understand the role, or anything else',
+	3, 3
+)
+
+INSERT INTO technical_documentation VALUES (
+4, '2004.04.04', 3, 2, 111, TRUE, 'This is gonna be a good idea', 5, 'Knows everything',
+	'He can turn on the computer',
+	'He can turn off the computer',
+	'Actually (s)he is a girl',
+	5,3,4,5,
+	7,
+	'Actually he can play the piano',
+	'I think he should be the boss because he knows everything about the role',
+	5, 5
+)
+
+INSERT INTO technical_documentation VALUES (
+5, '2022.11.10', 2, 1, 95, FALSE, 'Mister Architect', 4, 'MAXIMUM level',
+	'He can do everything',
+	'He can do programming in C',
+	'He does know even C++',
+	4,4,4,5,
+	9,
+	'Actually he can play the guitar',
+	'He does know everything, he is John Snow',
+	5, 2
+)
