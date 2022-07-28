dbf96bfa53707370537c30d999a878e669b33f59
Meszaros Tamas <tamasmt0154@gmail.com>
2022-07-28 12:32:43 +0200

    added manageDoc.sql to scripts

---
+++ scripts/manageDoc.sql
@@ -0,0 +1,29 @@
+INSERT INTO management_documentation VALUES(
+1, '2022-01-13', FALSE, TRUE, 'learning the usage of react', 'Frontend developer', 'he fits well enough',
+'high',  'very proactive', 'none', 'he fits into the team as a frontend candidate', 'he has a lot of motivation for the position',
+'excellent knowlegde in HTML and Javascript', 'lack of knowlege when it comes to react', 1, 2, TRUE
+)
+
+INSERT INTO management_documentation VALUES(
+1, '2022-01-15', TRUE, TRUE, 'to study spring boot', 'backend developer', 'she fits',
+'avarege',  'not proactive at all', 'none', 'she does not fit', 'lack of overall experience ',
+'avarege java knowlegde', '', 1, 1, FALSE
+)
+
+INSERT INTO management_documentation VALUES(
+1, '2022-01-12', FALSE, TRUE, 'learning the usage of react', 'Frontend developer', 'she fits',
+'low',  'somewhat proactive and interested', 'she cant speak english', 'she fits into the team', '',
+'excellent knowlegde in HTML, Javascript, Angular and React', 'cant speak english', 2, 4, TRUE
+)
+
+INSERT INTO management_documentation VALUES(
+1, '2022-04-25', TRUE, TRUE, 'he want sto work in a multicultural enviroment', 'Frontend developer', 'he fits well enough',
+'high',  'very proactive', 'none', 'he fits into the team as a frontend candidate', 'he has a lot of experience',
+'his experience gives him an edge when it comes to frontend development', '', 3, 4, TRUE
+)
+
+INSERT INTO management_documentation VALUES(
+1, '2022-04-28', FALSE, TRUE, '', 'Fullstack programmer', 'he fits',
+'high',  'very proactive', 'none', 'he fits into the team', 'he is open for improving both his frontend and backend knowledge',
+'very skilled with spring boot ang react as well', 'lack of experience with agile', 3, 3, TRUE
+)
