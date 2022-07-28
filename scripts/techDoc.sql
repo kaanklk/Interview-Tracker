af32179a481cc904719adfd266dcaef4d3bb8bc5
Krisztian Horvath <krissz@gmail.com>
2022-07-28 11:11:51 +0200

    updates

--- scripts/techDoc.sql
+++ scripts/techDoc.sql
@@ -1,5 +1,5 @@
 INSERT INTO technical_documentation VALUES (
-1, '2000.12.12', 1, 2, 120, FALSE, 'This is the end of you', 3, 'Tech skill comment num1',
+1, '2000-12-12', 1, 2, 120, FALSE, 'This is the end of you', 3, 'Tech skill comment num1',
 	'Tech skill comment num2',
 	'Tech skill comment num3',
 	'Tech skill comment num4',
@@ -10,7 +10,7 @@ INSERT INTO technical_documentation VALUES (
 	1, 4
 )
 INSERT INTO technical_documentation VALUES (
-2, '2010.10.10', 1, 4, 170, TRUE, 'This is not the end of you', 5, 'High leveled',
+2, '2010-10-10', 1, 4, 170, TRUE, 'This is not the end of you', 5, 'High leveled',
 	'High',
 	'High',
 	'VERY High',
@@ -21,7 +21,7 @@ INSERT INTO technical_documentation VALUES (
 	2, 1
 )
 INSERT INTO technical_documentation VALUES (
-3, '2003.03.03', 2, 3, 200, FALSE, 'This is the biggest mistake', 3, 'Low leveled',
+3, '2003-03-03', 2, 3, 200, FALSE, 'This is the biggest mistake', 3, 'Low leveled',
 	'He can turn on the computer',
 	'Wonderful',
 	'VERY High',
@@ -33,7 +33,7 @@ INSERT INTO technical_documentation VALUES (
 )
 
 INSERT INTO technical_documentation VALUES (
-4, '2004.04.04', 3, 2, 111, TRUE, 'This is gonna be a good idea', 5, 'Knows everything',
+4, '2004-04-04', 3, 2, 111, TRUE, 'This is gonna be a good idea', 5, 'Knows everything',
 	'He can turn on the computer',
 	'He can turn off the computer',
 	'Actually (s)he is a girl',
@@ -45,7 +45,7 @@ INSERT INTO technical_documentation VALUES (
 )
 
 INSERT INTO technical_documentation VALUES (
-5, '2022.11.10', 2, 1, 95, FALSE, 'Mister Architect', 4, 'MAXIMUM level',
+5, '2022-11-10', 2, 1, 95, FALSE, 'Mister Architect', 4, 'MAXIMUM level',
 	'He can do everything',
 	'He can do programming in C',
 	'He does know even C++',
