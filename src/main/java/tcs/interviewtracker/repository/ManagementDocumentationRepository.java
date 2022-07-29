14f24324d61fc5b0b09c8a29bc069584ad6c1db2
Meszaros Tamas <tamasmt0154@gmail.com>
2022-07-29 11:32:07 +0200

    corrected management doc repo

--- src/main/java/tcs/interviewtracker/repository/ManagementDocumentationRepository.java
+++ src/main/java/tcs/interviewtracker/repository/ManagementDocumentationRepository.java
@@ -1,9 +1,14 @@
 package tcs.interviewtracker.repository;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
 
 import tcs.interviewtracker.persistence.ManagementDocumentation;
 
 public interface ManagementDocumentationRepository extends JpaRepository<ManagementDocumentation, Long> {
-
+    List<ManagementDocumentation> findByUuid(@Param("UUID") UUID uuid);
 }
