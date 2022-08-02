4117d4292a178b9ae6e9454e114a3323fa883ba2
shukhratojiev <steven.gerrard007@mail.ru>
2022-08-02 15:35:40 +0200

    return dto converted to entity

--- src/main/java/tcs/interviewtracker/controller/UserController.java
+++ src/main/java/tcs/interviewtracker/controller/UserController.java
@@ -1,9 +1,14 @@
 package tcs.interviewtracker.controller;
 
-
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,12 +25,15 @@ import org.springframework.web.bind.annotation.RestController;
 import tcs.interviewtracker.DTOs.UserDTO;
 import tcs.interviewtracker.exceptions.ResourceAlreadyExistsException;
 import tcs.interviewtracker.exceptions.ResourceNotFoundException;
+import tcs.interviewtracker.persistence.User;
 import tcs.interviewtracker.service.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private ModelMapper modelMapper;
     private UserService service;
 
     UserController(UserService service){
@@ -34,27 +43,46 @@ public class UserController {
     @GetMapping("")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    public List<UserDTO> getAllUsers(){
-        return service.getAllUsers();
+    public List<UserDTO> getAllUsers(
+        @RequestParam(required = false, defaultValue = "10") Integer pagesize,
+        @RequestParam(required = false, defaultValue = "0") Integer offset,
+        @RequestParam(required = false, defaultValue = "id") String orderBy,
+        @RequestParam(required = false, defaultValue = "ascending") String orderDirection){
+
+        Pageable page;
+
+        if (orderDirection.equals("ascending"))
+            page = PageRequest.of(offset, pagesize, Sort.by(orderBy).ascending());
+        else
+            page = PageRequest.of(offset, pagesize, Sort.by(orderBy).descending());
+
+
+        var usersDTO = service.getAllUsers(page).stream()
+        .map(this::entityToDto)
+        .collect(Collectors.toList());
+
+        return usersDTO;
     }
 
     @GetMapping("/{uuid}")
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     public UserDTO getUserById(@PathVariable UUID uuid) throws ResourceNotFoundException{
-        return service.getUserById(uuid);
+        return entityToDto(service.getUserById(uuid));
     }
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public UserDTO save(@RequestBody UserDTO userDTO) throws ResourceAlreadyExistsException{
-        return service.saveUser(userDTO);
+        User user = dtoToEntity(userDTO);
+        return entityToDto(service.saveUser(user));
     }
 
     @PutMapping("/{uuid}")
     @ResponseStatus(HttpStatus.OK)
     public UserDTO updateUserById(@PathVariable UUID uuid, @RequestBody UserDTO userDTO) throws ResourceNotFoundException{
-        return service.updateUser(uuid,userDTO);
+        User user = dtoToEntity(userDTO);
+        return entityToDto(service.updateUser(uuid,user));
     }
 
     @DeleteMapping("/{uuid}")
@@ -62,5 +90,13 @@ public class UserController {
     public void deleteUserById(@PathVariable UUID uuid) throws ResourceNotFoundException{
         service.deleteUser(uuid);
     }
+
+    public User dtoToEntity(UserDTO userDTO){
+        return modelMapper.map(userDTO, User.class);
+   }
+
+   public UserDTO entityToDto(User user){
+       return modelMapper.map(user, UserDTO.class);
+   }
 
 }
--- src/main/java/tcs/interviewtracker/controller/UserRolesController.java
+++ src/main/java/tcs/interviewtracker/controller/UserRolesController.java
@@ -1,8 +1,12 @@
 package tcs.interviewtracker.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,8 +19,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import tcs.interviewtracker.DTOs.RoleDTO;
 import tcs.interviewtracker.DTOs.UserRolesDTO;
 import tcs.interviewtracker.exceptions.ResourceNotFoundException;
+import tcs.interviewtracker.persistence.Role;
+import tcs.interviewtracker.persistence.UserRoles;
+import tcs.interviewtracker.repository.RoleRepository;
 import tcs.interviewtracker.service.UserRolesService;
 
 
@@ -24,6 +32,10 @@ import tcs.interviewtracker.service.UserRolesService;
 @RequestMapping("/users/{userUuid}/roles")
 public class UserRolesController {
 
+    @Autowired
+    private RoleRepository roleRepo;
+    @Autowired
+    private ModelMapper modelMapper;
     private UserRolesService service;
 
     UserRolesController(UserRolesService service){
@@ -41,25 +53,69 @@ public class UserRolesController {
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
     public UserRolesDTO getRoleForSpecificProject(@PathVariable UUID userUuid, @PathVariable UUID userRolesUuid) throws ResourceNotFoundException{
-        return service.getRoleForSpecificProject(userUuid, userRolesUuid);
+        return entityToDto(service.getRoleForSpecificProject(userUuid, userRolesUuid));
     }
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     public UserRolesDTO setRole(@RequestBody UserRolesDTO userRoleDTO){
-        return service.saveUserRole(userRoleDTO);
+
+        UserRoles userRoles = new UserRoles();
+
+        userRoles.setUserUuid(userRoleDTO.getUserUuid());
+        userRoles.setProjectUuid(userRoleDTO.getProjectUuid());
+
+        List<Role> roles = new ArrayList<>();
+
+        for(RoleDTO r : userRoleDTO.getRoles()){
+            roles.add(roleRepo.findByUuid(r.getUuid()).get());
+        }
+        userRoles.setRoles(roles);
+
+        UserRoles created = service.saveUserRole(userRoles);
+
+        List<RoleDTO> roleCreated = Arrays.asList(modelMapper.map(created.getRoles(), RoleDTO[].class));
+
+        UserRolesDTO createdDTO = new UserRolesDTO();
+        createdDTO.setUuid(created.getUuid());
+        createdDTO.setUserUuid(created.getUserUuid());
+        createdDTO.setProjectUuid(created.getProjectUuid());
+        createdDTO.setRoles(roleCreated);
+
+        return createdDTO;
     }
 
     @PutMapping("/{userRolesUuid}")
     @ResponseStatus(HttpStatus.OK)
-    public UserRolesDTO setNewRole(@PathVariable UUID userRolesUuid, @RequestBody UserRolesDTO userRoleDTO){
-        return service.setNewRoleforExistingUser(userRolesUuid, userRoleDTO);
+    public UserRolesDTO setNewRole(@PathVariable UUID userRolesUuid, @RequestBody UserRolesDTO userRoleDTO) throws ResourceNotFoundException{
+
+        UserRoles userRole = service.getRoleForSpecificProject(userRoleDTO.getUserUuid(), userRolesUuid);
+
+        List<Role> roles = new ArrayList<>();
+
+        for(RoleDTO r : userRoleDTO.getRoles()){
+           roles.add(roleRepo.findByUuid(r.getUuid()).get());
+        }
+
+        userRole.setRoles(roles);
+        UserRoles updated = service.setNewRoleforExistingUser(userRolesUuid, userRole);
+
+        userRoleDTO.setUuid(updated.getUuid());
+        return userRoleDTO;
     }
 
     @DeleteMapping("/{userRolesUuid}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteRolesForSpecificProject(@PathVariable UUID userRolesUuid) throws ResourceNotFoundException{
         service.deleteUserRole(userRolesUuid);
+    }
+
+    public UserRolesDTO entityToDto(UserRoles userRoles){
+        return modelMapper.map(userRoles, UserRolesDTO.class);
+    }
+
+    public UserRoles dtoToEntity(UserRolesDTO userRolesDTO){
+        return modelMapper.map(userRolesDTO, UserRoles.class);
     }
 
 }
